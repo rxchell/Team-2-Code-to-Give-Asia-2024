0e97aff4fb47beee80f6264450ec41af44c0d54a
Ritika Joshi <ritikajoshi0114@gmail.com>
2024-08-23 22:40:00 +0800

    Setup express and routes

---
+++ backend/routes/donationRoute.js
@@ -0,0 +1,21 @@
+import express  from 'express'
+
+const router = express.Router()
+
+// middleware that is specific to this router
+const timeLog = (req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+  }
+  router.use(timeLog)
+  
+  // define the home page route
+  router.get('/', (req, res) => {
+    res.send('Birds home page')
+  })
+  // define the about route
+  router.get('/about', (req, res) => {
+    res.send('About birds')
+  })
+  
+  export default router
