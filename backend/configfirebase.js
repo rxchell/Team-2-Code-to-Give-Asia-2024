a18a2fb52eee78218e17b60d441863770d72f31a
Ritika Joshi <ritikajoshi0114@gmail.com>
2024-08-23 22:48:14 +0800

    Various sanitsation

--- backend/configfirebase.js
+++ backend/configfirebase.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import 'dotenv/config'
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD67j4K-mYCf1wvXrW4MypsJeZFCs0wBeQ",
-  authDomain: "codetogive2024-96f53.firebaseapp.com",
-  projectId: "codetogive2024-96f53",
-  storageBucket: "codetogive2024-96f53.appspot.com",
-  messagingSenderId: "413236645295",
-  appId: "1:413236645295:web:8327cb005100eb0a70335a",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase and needed functions
