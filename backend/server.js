ea634f5648ea7f5ef2b86ab7cea02f0b53aa28ec
key <keyyew@gmail.com>
2024-08-23 23:20:17 +0800

    added errorMiddleware & async middleware

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
-import express  from 'express'
+import express from 'express'
 import userRoute from './routes/userRoute.js'
 import donationRoute from './routes/donationRoute.js'
 import orderRoute from './routes/orderRoute.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 const app = express()
 const PORT = 3000
 
-// respond with "hello world" when a GET request is made to the homepage
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('API is running...')
 })
 
 app.use('/api/users', userRoute)
 app.use('/api/donations', donationRoute)
 app.use('/api/orders', orderRoute)
 
+app.use(notFound)
+app.use(errorHandler)
+
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+  console.log(`app listening on port ${PORT}`)
 })
