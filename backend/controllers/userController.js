579bf7f562d2af0228c9477b128a548592cf39e4
key <keyyew@gmail.com>
2024-08-24 13:24:05 +0800

    remove login & logout, edited registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,63 +1,38 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import { auth } from "../configfirebase.js";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, firestore } from "../configfirebase.js";
 
-// @desc    login user & get token
-// @route   POST /api/users/login
-// @access  Public
-const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-
-    if (userCredential) {
-        const user = userCredential.user
-        const token = await user.getIdToken()
-
-        res.cookie('jwt', token, {
-            httpOnly: true, secure: true, sameSite: 'strict', maxAge: 5 * 60 * 1000 // 5 mins 
-        });
-        res.json({
-            uid: user.uid,
-            name: user.displayName,
-            email: user.email,
-            // TODO user type
-        })
-    }
-})
-
-// @desc    Logout user / clear cookie
-// @route   POST /api/users/logout
-// @access  Private
-const logoutUser = asyncHandler(async (req, res) => {
-    res.cookie('jwt', '', { httpOnly: true, expires: new Date(0) });
-    res.status(200).json({ message: 'Logged out successfully' })
-})
-
-// @desc    Register user
+// @desc    Register new user 
 // @route   POST /api/users
 // @access  Private/Admin
 const registerUser = asyncHandler(async (req, res) => {
     // TODO add other fields once form confirmed
-    const { name, email, password } = req.body;
+    const { name, email, password, phoneNumber, userRole } = req.body;
 
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    // TODO link up details from user collection
+    const user = await auth.createUser({
+        displayName: name,
+        email: email,
+        phoneNumber: phoneNumber,
+        password: password
+    });
 
-    if (userCredential) {
-        const user = userCredential.user
-        const token = await user.getIdToken()
+    if (user) {
+        const userId = user.uid
+        const userDocRef = firestore.collection('users').doc(userId)
 
-        res.cookie('jwt', token, {
-            httpOnly: true, secure: true, sameSite: 'strict', maxAge: 5 * 60 * 1000 // 5 mins 
-        });
+        // create role for user, default is individual
+        await auth.createCustomToken(userId, { userRole: userRole || "individual" });
+
+        // stores user information
+        userDocRef.set({
+            name: name,
+            email: email,
+            phoneNumber: phoneNumber
+            // TODO include other information once confirmed
+        })
         res.json({
-            uid: user.uid,
-            name: user.displayName,
-            email: user.email,
-            // TODO user type
+            message: "user registered successfully"
         })
     }
 })
 
-export { loginUser, logoutUser, registerUser }
+export { registerUser }
