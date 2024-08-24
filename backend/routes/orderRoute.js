7f5e6142692de60b2794faa83afc882efc37b80f
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2024-08-24 13:58:15 +0800

    Add endpoints and routes for orders

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,21 +1,14 @@
-import express  from 'express'
+import express from 'express';
+import { getAllOrders, getOrderById, updateOrderById, deleteOrderById, createOrder } from '../controllers/orderController.js';
+import asyncHandler from "../middleware/asyncHandler.js";
 
-const router = express.Router()
+const router = express.Router();
 
-// middleware that is specific to this router
-const timeLog = (req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
-  }
-  router.use(timeLog)
-  
-  // define the home page route
-  router.get('/', (req, res) => {
-    res.send('Birds home page')
-  })
-  // define the about route
-  router.get('/about', (req, res) => {
-    res.send('About birds')
-  })
-  
-  export default router
+// Define routes and map them to controller functions
+router.get('/', asyncHandler(getAllOrders)); // Get all orders
+router.get('/:id', asyncHandler(getOrderById)); // Get order by ID
+router.put('/:id', asyncHandler(updateOrderById)); // Update order by ID
+router.delete('/:id', asyncHandler(deleteOrderById)); // Delete order by ID
+router.post('/', asyncHandler(createOrder)); // Create a new order
+
+export default router;
