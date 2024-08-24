f662c3711e7b976d08fe4d8c3274322b7912864d
Wx <26271681+JamesCrr@users.noreply.github.com>
2024-08-24 13:16:49 +0800

    Update donationRoute.js

--- backend/routes/donationRoute.js
+++ backend/routes/donationRoute.js
@@ -1,21 +1,87 @@
-import express  from 'express'
+import express from "express";
+import { collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc } from "firebase/firestore";
+import { firestore } from "../configfirebase.js";
 
-const router = express.Router()
+const router = express.Router();
+const collectionName = "donations";
 
-// middleware that is specific to this router
-const timeLog = (req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
+// // middleware that is specific to this router
+// const timeLog = (req, res, next) => {
+//   console.log("Time: ", Date.now());
+//   next();
+// };
+// router.use(timeLog);
+
+// Get
+router.get("/get", async (req, res) => {
+  const docRef = doc(firestore, collectionName, req.query.foodDonationID);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // docSnap.data() will be undefined
+    res.json({ errorMessage: `${req.query.foodDonationID} not found` });
+    return;
   }
-  router.use(timeLog)
-  
-  // define the home page route
-  router.get('/', (req, res) => {
-    res.send('Birds home page')
-  })
-  // define the about route
-  router.get('/about', (req, res) => {
-    res.send('About birds')
-  })
-  
-  export default router
+
+  res.json(docSnap.data());
+});
+
+// Get All
+router.get("/getall", async (req, res) => {
+  let returnResult = {};
+
+  const querySnapshot = await getDocs(collection(firestore, collectionName));
+  querySnapshot.forEach((doc) => {
+    returnResult[doc.id] = doc.data();
+  });
+
+  res.json(returnResult);
+});
+
+// Update
+router.post("/update", async (req, res) => {
+  if (!req.body.donationID) {
+    res.json({ errorMessage: `donationID not found in object! Please pass donationID in the object` });
+    return;
+  }
+  const docRef = doc(firestore, collectionName, req.body.donationID);
+
+  try {
+    await updateDoc(docRef, req.body);
+  } catch (error) {
+    res.json({ errorMessage: `Error updating document: ${error}` });
+    return;
+  }
+  res.json(req.body);
+});
+
+// Delete
+router.delete("/delete", async (req, res) => {
+  if (!req.body.donationID) {
+    res.json({ errorMessage: `donationID not found in object! Please pass donationID in the object` });
+    return;
+  }
+  try {
+    await deleteDoc(doc(firestore, collectionName, req.body.donationID));
+  } catch (error) {
+    res.json({ errorMessage: `Error deleting document: ${error}` });
+    return;
+  }
+  res.json({ message: "success" });
+});
+
+// Create
+router.put("/create", async (req, res) => {
+  try {
+    const docRef = await addDoc(collection(firestore, collectionName), req.body);
+    console.log("Document written with ID: ", docRef.id);
+    res.json({ [docRef.id]: req.body });
+  } catch (error) {
+    res.json({ errorMessage: `Error adding document: ${error}` });
+    return;
+  }
+});
+
+export default router;
