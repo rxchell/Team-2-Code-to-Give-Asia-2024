bf81c84d5ab5bae915bdde7577bd02b69fcd523a
JUIN TAN <tanjuinlong@gmail.com>
2024-08-24 18:50:28 +0800

    adjust layout

--- frontend/src/utils/Layout.jsx
+++ frontend/src/utils/Layout.jsx
@@ -7,10 +7,7 @@ export default function Layout() {
     return (
         <div className="p-0 flex flex-col min-h-screen bg-gray-200">
             <Header />
-            <div className="flex">
-                <Sidebar />
-                <Outlet />
-            </div>
+            <Outlet />
             <Footer />
         </div>
     );
