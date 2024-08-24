c4a8983c0d3b357ada5c01fc1830b6e298823546
sotongsamho <samiha12018@gmail.com>
2024-08-24 16:42:30 +0800

    made the form but need to style it

--- frontend/src/utils/Layout.jsx
+++ frontend/src/utils/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div className="p-0 flex flex-col min-h-screen bg-yellow-100">
+        <div className="p-0 flex flex-col min-h-screen bg-[#F4F5F4]">
             <Header userRole={'agency'}/>
             <Outlet />
             <Footer />
