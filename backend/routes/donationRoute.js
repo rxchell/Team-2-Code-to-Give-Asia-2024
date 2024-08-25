0209d6fb7948fb4a7c3b6d3a83e2508f6a6aed4d
key <keyyew@gmail.com>
2024-08-25 22:13:14 +0800

    created image upload for donation

--- backend/routes/donationRoute.js
+++ backend/routes/donationRoute.js
@@ -3,14 +3,16 @@ import express from "express";
 import { createDonation, deleteDonation, getDonationByID, getDonations, updateDonation, getDonationsByUserID } from "../controllers/donationController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
+import multer from 'multer';
 
+var upload = multer({ dest: "./uploads" }).single('imageURL')
 const router = express.Router();
 
 router.get("/:id", getDonationByID);
 router.route("/").get(protect, admin, getDonations);
 // router.route("/").get(getDonations);
 router.get("/user/:id", getDonationsByUserID);
-router.post("/", createDonation);
+router.post("/", upload, createDonation);
 router.delete("/:id", deleteDonation);
 router.put("/:id", updateDonation);
 
