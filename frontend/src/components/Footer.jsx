86ba5f3cdcd544af727d84b587da6f8d7f2ad673
JUIN TAN <tanjuinlong@gmail.com>
2024-08-26 00:23:30 +0800

    fix footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -38,18 +38,13 @@ export default function Footer() {
               </svg>
             </a> */}
             <a href="https://www.instagram.com/foodbanksg/?hl=en" class="inline-block p-4 bg-transparent text-gray-50 hover:bg-gray-50 hover:text-gray-600 transition-colors duration-300 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" className=" size-6 fill-current">
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-6 fill-current">
                 <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"></path>
               </svg>
             </a>
 
             <a href="https://www.facebook.com/thefoodbanksingapore/" class="inline-block p-4 bg-transparent text-gray-50 hover:bg-gray-50 hover:text-gray-600 transition-colors duration-300 rounded-full">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current">
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-6 fill-current">
                 <path
                   d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
               </svg>
