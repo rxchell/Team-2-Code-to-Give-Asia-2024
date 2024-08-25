d00dc5a67516aee45c717f72723e04892aa37e6b
key <keyyew@gmail.com>
2024-08-26 00:36:02 +0800

    edited donationRoute

--- backend/routes/donationRoute.js
+++ backend/routes/donationRoute.js
@@ -6,10 +6,10 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.get("/user", protect, getDonationsByUserID);
 router.get("/:id", getDonationByID);
 router.route("/").get(protect, admin, getDonations);
 // router.route("/").get(getDonations);
-router.get("/user", protect, getDonationsByUserID);
 router.post("/", createDonation);
 router.delete("/:id", deleteDonation);
 router.put("/:id", updateDonation);
