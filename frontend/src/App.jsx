12be7251c5051bc6158045469b78aeaeae6a297c
sotongsamho <samiha12018@gmail.com>
2024-08-25 11:56:57 +0800

    fixed form to maintain state accross pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import RegisterPage from './pages/RegisterPage'
 import DonatePage from './pages/DonatePage'
 import GuidelinesPage from './pages/GuidelinesPage'
 import AdditionalInfoPage from './pages/AdditionalInfoPage'
+import { FormProvider } from './utils/FormContext'
 
 // axios.defaults.baseURL = "http://localhost:3000/";
 // axios.defaults.withCredentials = true;
@@ -17,6 +18,7 @@ import AdditionalInfoPage from './pages/AdditionalInfoPage'
 export default function App() {
     return (
         // <UserContextProvider>
+        <FormProvider>
             <Routes>
                 <Route path="/" element={<Layout />} >
                     <Route index element={<HomePage />} />
@@ -28,6 +30,8 @@ export default function App() {
                     <Route path='*' element={<h1>Not Found</h1>} />
                 </Route>
             </Routes>
+        </FormProvider>
+            
         // </UserContextProvider>
     )
 }
